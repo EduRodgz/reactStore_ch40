afd3584df8044b5b504abccda5c5994020aaa35c
EduRodgz <edurodgz.usmc@icloud.com>
2023-09-26 19:01:29 -0500

    Tuesday first commit

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -37,6 +37,25 @@ function Admin() {
           <label className="form-label">Discount</label>
           <input type="text" className="form-control" />
         </div>
+        <div className="form">
+          <h3>Donation</h3>
+          <div class="dropdown">
+  <button class="btn btn-success dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    Charity
+  </button>
+  <ul class="dropdown-menu dropdown-menu-dark">
+    <li><a class="dropdown-item" href="#">Army</a></li>
+    <li><a class="dropdown-item" href="#">Marines</a></li>
+    <li><a class="dropdown-item" href="#">Navy</a></li>
+    <li><a class="dropdown-item" href="#">Air Force</a></li>
+    <li><a class="dropdown-item" href="#">Space Force</a></li>
+  </ul>
+</div>
+        <div>
+    <label className="form-label">Amount</label>
+    <input type="number" className="form-control"/>
+        </div>
+</div>
       </div>
     </div>
   );

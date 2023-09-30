5f99e2de6cd437d21db0ab4524fbe0979e60f4bd
EduRodgz <edurodgz.usmc@icloud.com>
2023-09-30 14:42:37 -0500

    last commit

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -8,7 +8,23 @@ function About(){
         <hr/>
         <h5>Created by:</h5>
         <h1>Eduardo Rodriguez</h1>
-    </div>
+        <p className="para">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates aliquid eveniet modi itaque ipsa quae rerum at nisi adipisci ducimus voluptatum officiis quibusdam tenetur, cupiditate nihil id magnam placeat a! Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi doloribus cupiditate qui cum, unde in nemo incidunt praesentium deleniti, nobis, suscipit ad eum eligendi officia quod molestias tempora sed possimus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae reiciendis corrupti saepe eos ex praesentium nobis, culpa alias nihil nesciunt maiores quas laboriosam labore provident inventore quod dolore repellendus! Inventore.
+
+        </p>
+        <img className="image-source" src="./images/texas2.webp" alt="texas2" />
+        <p className="para">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates aliquid eveniet modi itaque ipsa quae rerum at nisi adipisci ducimus voluptatum officiis quibusdam tenetur, cupiditate nihil id magnam placeat a! Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi doloribus cupiditate qui cum, unde in nemo incidunt praesentium deleniti, nobis, suscipit ad eum eligendi officia quod molestias tempora sed possimus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae reiciendis corrupti saepe eos ex praesentium nobis, culpa alias nihil nesciunt maiores quas laboriosam labore provident inventore quod dolore repellendus! Inventore.
+
+        </p>
+        <img className="image-source" src="./images/drp.webp" alt="drp" />
+        <p className="para">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates aliquid eveniet modi itaque ipsa quae rerum at nisi adipisci ducimus voluptatum officiis quibusdam tenetur, cupiditate nihil id magnam placeat a! Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi doloribus cupiditate qui cum, unde in nemo incidunt praesentium deleniti, nobis, suscipit ad eum eligendi officia quod molestias tempora sed possimus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae reiciendis corrupti saepe eos ex praesentium nobis, culpa alias nihil nesciunt maiores quas laboriosam labore provident inventore quod dolore repellendus! Inventore.
+
+        </p>
+        <img className="image-source" src="./images/gun.jpg" alt="gun" />
+        <p className="para">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates aliquid eveniet modi itaque ipsa quae rerum at nisi adipisci ducimus voluptatum officiis quibusdam tenetur, cupiditate nihil id magnam placeat a! Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi doloribus cupiditate qui cum, unde in nemo incidunt praesentium deleniti, nobis, suscipit ad eum eligendi officia quod molestias tempora sed possimus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae reiciendis corrupti saepe eos ex praesentium nobis, culpa alias nihil nesciunt maiores quas laboriosam labore provident inventore quod dolore repellendus! Inventore.
+
+        </p>
+        </div>
+        
     );
 } 
 
